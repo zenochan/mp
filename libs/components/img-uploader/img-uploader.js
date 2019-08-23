85d8ebab984efcbca735f6e4320de625cbe2f644
Zeno <zenochan@qq.com>
2019-08-23 09:49:25 +0800

    quick_push

---
+++ libs/components/img-uploader/img-uploader.js
@@ -0,0 +1,86 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * # properties
+ *
+ * - cameraOnly 仅允许相机拍照
+ * - count 最大数量，默认9
+ * - text 上传按钮提示文字
+ * - disabled 只读
+ *
+ * # events
+ * - change 数据改变
+ */
+var api_service_1 = require("../../service/api.service");
+var UI_1 = require("../../wx/UI");
+var WX_1 = require("../../wx/WX");
+exports.IMG_UPLOAD = {
+    deleteUrl: "delete"
+};
+Component({
+    data: {
+        uploading: [],
+    },
+    options: {
+        addGlobalClass: true
+    },
+    properties: {
+        urls: {
+            type: Array, value: [], observer: function (newVal) {
+                if (!newVal)
+                    this.setData({ urls: [] });
+            }
+        },
+        disabled: { type: Boolean, value: false },
+        /** 最大数量, 默认 9 */
+        count: { type: Number, value: 9 },
+        /** 是否允许从相册选择 */
+        cameraOnly: { type: Boolean, value: false },
+        text: { type: String, value: "上传作品" }
+    },
+    methods: {
+        chooseImage: function () {
+            var _this = this;
+            var from = ['camera'];
+            if (!this.data.cameraOnly) {
+                from.unshift("album");
+            }
+            WX_1.WX.chooseImage(this.data.count - this.data.urls.length, from)
+                .flatMap(function (filePaths) {
+                _this.setData({ uploading: filePaths });
+                return api_service_1.API.uploadMore(filePaths);
+            }) // 上传
+                .subscribe(function (res) {
+                var _a;
+                (_a = _this.data.urls).push.apply(_a, res);
+                _this.setData({
+                    urls: _this.data.urls,
+                    uploading: []
+                });
+                _this.triggerEvent("change", { value: _this.data.urls });
+            }, function (e) {
+                if (typeof e == "string")
+                    UI_1.UI.toastFail(e, 2000);
+                _this.setData({ uploading: [] });
+            });
+        },
+        removeImage: function (event) {
+            var _this = this;
+            UI_1.UI.confirm("是否要删除图片?").subscribe(function (res) {
+                var deleted = _this.data.urls.splice(event.currentTarget.dataset.index, 1);
+                api_service_1.API.post(deleted, { photos: deleted }).subscribe(function (res) { }, function (e) { return console.error("图片删除失败", e); });
+                _this.setData({ urls: _this.data.urls });
+                _this.triggerEvent("change", { value: _this.data.urls });
+            });
+        },
+        view: function (e) {
+            var url = e.currentTarget.dataset.url;
+            var urls = e.currentTarget.dataset.urls;
+            wx.previewImage({
+                current: url,
+                urls: urls
+            });
+        }
+    },
+});
+//# sourceMappingURL=img-uploader.js.map
---
+++ libs/components/img-uploader/img-uploader.ts
@@ -0,0 +1,87 @@
+/**
+ * # properties
+ *
+ * - cameraOnly 仅允许相机拍照
+ * - count 最大数量，默认9
+ * - text 上传按钮提示文字
+ * - disabled 只读
+ *
+ * # events
+ * - change 数据改变
+ */
+import {API} from "../../service/api.service";
+import {UI} from "../../wx/UI";
+import {WX} from "../../wx/WX";
+
+export let IMG_UPLOAD = {
+  deleteUrl: "delete"
+};
+
+Component({
+  data: {
+    uploading: [],
+  },
+
+  options: {
+    addGlobalClass: true
+  },
+
+  properties: {
+    urls: {
+      type: Array, value: [], observer: function (newVal) {
+        if (!newVal) this.setData({urls: []});
+      }
+    },
+    disabled: {type: Boolean, value: false},
+    /** 最大数量, 默认 9 */
+    count: {type: Number, value: 9},
+    /** 是否允许从相册选择 */
+    cameraOnly: {type: Boolean, value: false},
+    text: {type: String, value: "上传作品"}
+  },
+
+
+  methods: {
+    chooseImage()
+    {
+      let from = ['camera'];
+      if (!this.data.cameraOnly) { from.unshift("album") }
+      WX.chooseImage(this.data.count - this.data.urls.length, from)
+          .flatMap(filePaths => {
+            this.setData({uploading: filePaths});
+            return API.uploadMore(filePaths)
+          }) // 上传
+          .subscribe(res => {
+            this.data.urls.push(...res);
+            this.setData({
+              urls: this.data.urls,
+              uploading: []
+            });
+            this.triggerEvent("change", {value: this.data.urls})
+          }, e => {
+            if (typeof e == "string") UI.toastFail(e, 2000);
+            this.setData({uploading: []});
+          });
+    },
+
+    removeImage(event: WXEvent)
+    {
+      UI.confirm("是否要删除图片?").subscribe(res => {
+        let deleted = this.data.urls.splice(event.currentTarget.dataset.index, 1);
+        API.post(deleted, {photos: deleted}).subscribe(res => {}, e => console.error("图片删除失败", e));
+        this.setData({urls: this.data.urls});
+        this.triggerEvent("change", {value: this.data.urls})
+      });
+    },
+
+    view(e: WXEvent)
+    {
+      let url = e.currentTarget.dataset.url;
+      let urls = e.currentTarget.dataset.urls;
+      wx.previewImage({
+        current: url,
+        urls: urls
+      });
+    }
+  },
+});
