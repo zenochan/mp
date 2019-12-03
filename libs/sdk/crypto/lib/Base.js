8d29368a35fc0da7a4d11e6c2d5f4c8c3e2f3b2d
Zeno <zenochan@qq.com>
2019-12-03 11:24:00 +0800

    quick_push

--- libs/sdk/crypto/lib/Base.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Base = /** @class */ (function () {
-    function Base() {
-    }
-    return Base;
-}());
-exports.Base = Base;
-//# sourceMappingURL=Base.js.map
