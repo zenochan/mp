ab0f34d51d157bfe6b8fdc15306135c469f2818e
Zeno <zenochan@qq.com>
2021-06-04 14:48:28 +0800

    feat: publish v1.0.0

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'extends': [
+    'ts-base',
+    'airbnb',
+  ],
+  'rules': {
+    'prefer-destructuring': 1,
+    'no-param-reassign': 1,
+    'no-undef': 1,
+    'prefer-rest-params': 1,
+    'no-plusplus': 1,
+    'eqeqeq': 1,
+    'no-use-before-define': 1,
+    'max-len': [
+      2,
+      120,
+    ],
+    'no-tabs': 0,
+    'no-multi-spaces': 0,
+  },
+};
